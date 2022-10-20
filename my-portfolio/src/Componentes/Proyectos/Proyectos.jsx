07484db25753c5a9f7cf695066a8aff2e7bd4dec
santiVarese <107406089+santiVarese@users.noreply.github.com>
2022-10-20 20:32:39 -0300

    cuartoCommit

--- my-portfolio/src/Componentes/Proyectos/Proyectos.jsx
+++ my-portfolio/src/Componentes/Proyectos/Proyectos.jsx
@@ -2,20 +2,6 @@ import React from "react";
 import "./Proyectos.css";
 import { Link } from "react-router-dom";
 import NavBar from "../../NavBar/NavBar";
-import imagenPagina1 from "../../Materiales/imagenPagina1.png";
-import imagenPagina2 from "../../Materiales/imagenPagina2.png";
-import imagenPagina3 from "../../Materiales/imagenPagina3.png";
-import imagenPagina4 from "../../Materiales/imagenPagina4.png";
-import modalWeb from "../../Materiales/modalWeb.png";
-import responsive1 from "../../Materiales/responsive1.png";
-import responsive2 from "../../Materiales/responsive2.png";
-import responsive3 from "../../Materiales/responsive3.png";
-import responsive4 from "../../Materiales/responsive4.png";
-import responsive5 from "../../Materiales/responsive5.png";
-import responsive6 from "../../Materiales/responsive6.png";
-import responsive7 from "../../Materiales/responsive7.png";
-import responsive8 from "../../Materiales/responsive8.png";
-import modalResponsive from "../../Materiales/modalResponsive.png";
 import videoPrimerProyecto from "../../Materiales/videoPrimerProyecto.mp4";
 
 function Proyectos() {
